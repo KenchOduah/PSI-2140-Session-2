424c75649ac4ceb23dacc0a468ae7f1ef7f4c62f
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-02-02 14:21:18 -0500

    Add files via upload

---
+++ PSI2140Session2.java
@@ -0,0 +1,35 @@
+package Practice;
+
+public class PSI2140Session2 {
+	
+	public static void main(String[] args) {
+		int i = 100;
+		String s = "20";
+		double d = 30.50;
+		boolean t = true;
+		double f = (int)20.4;
+		char c = 'c';
+		int p = 50;
+		
+		//System.out.println(i * f);
+		//System.out.println(i / f);
+		//System.out.println(d + f);
+		//System.out.println(s + d);
+		//System.out.println((i + p) / f);
+		
+		/*if ((i > p) && (!t)) {
+			System.out.println("Hello");
+		}
+		else {
+			System.out.println("Goodbye");
+		}
+		
+		if ((i > p) || (!t)) {
+			System.out.println("Hello");
+		}
+		else {
+			System.out.println("Goodbye");
+		}*/
+		
+	}
+}
